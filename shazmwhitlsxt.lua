6639ca3e94e718f7a9b0720271aa88ec4b7a1207
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-04-06 21:55:15 -0400

    Update shazmwhitlsxt.lua

--- shazmwhitlsxt.lua
+++ shazmwhitlsxt.lua
@@ -1,1 +1,29 @@
 
+			local ejfgbdjs = game.Players
+local SDIKjFNIEJSMNFdiks = {
+    "7913B3BF-1182-48EC-A09D-1228A120C34F", -- trueonezz
+    "2689D387-B14E-4F03-8FD1-F006CEFE534B", -- dexx
+    "8A7A2F73-0EC1-4B73-8B6D-BD55E5C3F6DF", -- jacob
+    "", -- preson
+    "", -- anx
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    ""
+}
+if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
+    
+    else
+    ejfgbdjs.LocalPlayer:Kick("Error: 624")
+end
