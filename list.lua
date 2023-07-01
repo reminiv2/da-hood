e679c515368e8fa4596d22653c5a64063490eaf4
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-07-01 05:51:44 -0400

    Update list.lua

--- list.lua
+++ list.lua
@@ -2,13 +2,13 @@
 local Hwid = game:GetService("RbxAnalyticsService"):GetClientId()
 --// players \\--
 local p1 = "29377DD8-B8B3-40CB-ABA0-BA4E23609EB4" -- hwid
-local p2 = "" -- hwid
+local p2 = "DE22E2DA-8B52-47CC-ADF2-FECDFE4D103D" -- hwid
 local p3 = "" -- hwid
 --// keys \\--
-if p1 == Hwid and _G.Key == "111" then
+if p1 == Hwid and _G.Key == "gm49tjefbnuewjfbsdjcfndskcfgedf" then
     print("key is good and hwid")
 elseif
- p2 == Hwid and _G.Key == "222" then
+ p2 == Hwid and _G.Key == "4rw8ehfsudfhasiknd47rhwidfdfhew8" then
     print("key is good and hwid")
 elseif
  p3 == Hwid and _G.Key == "333" then
