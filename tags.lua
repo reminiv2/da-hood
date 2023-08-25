64ab2d4b99f5de64c801d2715c8ab01eca8f93b8
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-25 16:40:59 -0400

    Create tags.lua

---
+++ tags.lua
@@ -0,0 +1,36 @@
+getgenv().OverSeer = "sp0zx"
+getgenv().WaveOwner1 = "o1ekv"
+getgenv().WaveOwner2 = "Vesperg777"
+getgenv().Clown1 = "HeavenLimitless"
+getgenv().Clown2 = "NotSlpher"
+
+repeat wait()
+if game.Players:FindFirstChild(getgenv().OverSeer) then
+    game.Players[getgenv().OverSeer].Character.Humanoid.DisplayName = ('[ Wave OverSeer 🌐 ] ' .. game.Players[getgenv().OverSeer].DisplayName)
+end
+until game.Players:FindFirstChild(getgenv().OverSeer)
+--------------------------------------------------------------------------------------------
+repeat wait()
+if game.Players:FindFirstChild(getgenv().WaveOwner1) then
+    game.Players[getgenv().WaveOwner1].Character.Humanoid.DisplayName = ('[ Wave Owner 🔥 ] ' .. game.Players[getgenv().WaveOwner1].DisplayName)
+end
+until game.Players:FindFirstChild(getgenv().WaveOwner1)
+--------------------------------------------------------------------------------------------
+repeat wait()
+if game.Players:FindFirstChild(getgenv().WaveOwner2) then
+    game.Players[getgenv().WaveOwner2].Character.Humanoid.DisplayName = ('[ Wave Owner 🔥 ] ' .. game.Players[getgenv().WaveOwner2].DisplayName)
+end
+until game.Players:FindFirstChild(getgenv().WaveOwner2)
+--------------------------------------------------------------------------------------------
+repeat wait()
+if game.Players:FindFirstChild(getgenv().Clown1) then
+    game.Players[getgenv().Clown1].Character.Humanoid.DisplayName = ('[ Wave Mod 🤡 ] ' .. game.Players[getgenv().Clown1].DisplayName)
+end
+until game.Players:FindFirstChild(getgenv().Clown1)
+--------------------------------------------------------------------------------------------
+repeat wait()
+if game.Players:FindFirstChild(getgenv().Clown2) then
+    game.Players[getgenv().Clown2].Character.Humanoid.DisplayName = ('[ Wave Mod 🤡 ] ' .. game.Players[getgenv().Clown2].DisplayName)
+end
+until game.Players:FindFirstChild(getgenv().Clown2)
+--------------------------------------------------------------------------------------------
