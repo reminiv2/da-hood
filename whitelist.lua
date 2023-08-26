d59f6c8a6c6c1827ea95214112dcbf30492912b0
reminiv2 <121924130+reminiv2@users.noreply.github.com>
2023-08-26 13:18:59 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,18 +1,13 @@
 local ejfgbdjs = game.Players
 local SDIKjFNIEJSMNFdiks = {
-    "C2EE9A0D-AB0B-4271-B50C-4DF21EA92881", -- trueonezz
-    "29377DD8-B8B3-40CB-ABA0-BA4E23609EB4", -- dexx
-    "E639F829-98D6-4F17-BE7E-A7F1F6E17A19", -- batoxa (buyer)
-    "402D2B02-1BA4-4DB3-8A16-EB773B6E5A79", -- anx (buyer)
-    "F3D680D9-216F-4882-8BCF-AC0DFC5438DF", -- YK (buyer)
-    "9596EF9B-5EE6-48A3-9588-D0739B851282", -- ter (buyer)
-    "CCE15A56-3503-4FF1-A373-6A45FE99BCB4", -- Far (buyer)
-    "AE1B70D4-4E81-44AB-9DB9-6B8220ECD80B", -- 5.iz (buyer)
-    "67A66F6F-BAB8-41A0-943E-71C9236F5CC9", -- blade (buyer)
-    "20A421CB-27B7-4D5A-A697-600127B7A38B", -- edis (buyer)
-    "2C94A55C-665E-4ACB-8EEB-034050DCD80D", -- Slpher (buyer)
-    "282e86ca-29e7-4a66-937e-dd2a108a28d8" -- choppa (buyer|account)
-    
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
 }
 if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
     
